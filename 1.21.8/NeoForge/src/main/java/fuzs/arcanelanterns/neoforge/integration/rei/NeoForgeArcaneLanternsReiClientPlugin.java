c5907b0b8c2e2e5574068dd70f9f478a1c2a2146
Fuzss <28218241+Fuzss@users.noreply.github.com>
2025-07-27 19:33:52 +0200

    enable REI again

--- 1.21.8/NeoForge/src/main/java/fuzs/arcanelanterns/neoforge/integration/rei/NeoForgeArcaneLanternsReiClientPlugin.java
+++ 1.21.8/NeoForge/src/main/java/fuzs/arcanelanterns/neoforge/integration/rei/NeoForgeArcaneLanternsReiClientPlugin.java
@@ -1,9 +1,9 @@
-//package fuzs.arcanelanterns.neoforge.integration.rei;
-//
-//import fuzs.arcanelanterns.integration.rei.ArcaneLanternsReiClientPlugin;
-//import me.shedaniel.rei.forge.REIPluginClient;
-//
-//@REIPluginClient
-//public class NeoForgeArcaneLanternsReiClientPlugin extends ArcaneLanternsReiClientPlugin {
-//
-//}
+package fuzs.arcanelanterns.neoforge.integration.rei;
+
+import fuzs.arcanelanterns.integration.rei.ArcaneLanternsReiClientPlugin;
+import me.shedaniel.rei.forge.REIPluginClient;
+
+@REIPluginClient
+public class NeoForgeArcaneLanternsReiClientPlugin extends ArcaneLanternsReiClientPlugin {
+
+}
